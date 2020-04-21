d9478c754eece0704c3c82a00ea3c3010e699a55
GazAppy <63299214+GazAppy@users.noreply.github.com>
2020-04-21 15:01:42 +0100

    Update contact_me.js

--- js/contact_me.js
+++ js/contact_me.js
@@ -21,25 +21,16 @@ $(function() {
       
           $.ajax({
             <form id="my-form"
-  action="https://formspree.io/mpzjgvdq"
-  method="POST"
->
-  <label>Email:</label>
-  <input type="email" name="email" />
-  <label>Message:</label>
-  <input type="text" name="message" />
-  <button id="my-form-button">Submit</button>
-  <p id="my-form-status"></p>
-</form>
-                url: "https://formspree.io/mpzjgvdq",
-                type: "POST",
-                data: {
+            action="https://formspree.io/mpzjgvdq"
+            method="POST"
+            data: {
                     name: name,
                     phone: phone,
                     email: email,
                     message: message},
-        
-        cache: true,
+        </form>
+                 })
+        cache: false,
         success: function() {
           // Success message
           $('#success').html("<div class='alert alert-success'>");
@@ -52,7 +43,7 @@ $(function() {
           //clear all fields
           $('#contactForm').trigger("reset");
         },
-        cache: false,
+        
         error: function() {
           // Fail message
           $('#success').html("<div class='alert alert-danger'>");
