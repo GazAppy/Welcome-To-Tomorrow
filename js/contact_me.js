ba966dbff10d1168d69ffee12ad08111d15669d9
GazAppy <63299214+GazAppy@users.noreply.github.com>
2020-04-21 14:30:59 +0100

    Update contact_me.js

--- js/contact_me.js
+++ js/contact_me.js
@@ -17,8 +17,12 @@ $(function() {
       if (firstName.indexOf(' ') >= 0) {
         firstName = name.split(' ').slice(0, -1).join(' ');
       }
-     
-   
+      <form action="https://formspree.io/mpzjgvdq" method="POST">
+  <input type="text" name="name">
+  <input type="email" name="_replyto">
+  <input type="submit" value="Send">
+</form
+      
           $.ajax({
                 url: "https://formspree.io/mpzjgvdq",
                 type: "POST",
