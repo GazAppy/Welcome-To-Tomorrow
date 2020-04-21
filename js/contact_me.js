27e5b871cf61fba4999c9655db1d9555f85555d9
GazAppy <63299214+GazAppy@users.noreply.github.com>
2020-04-21 12:16:06 +0100

    Update contact_me.js

--- js/contact_me.js
+++ js/contact_me.js
@@ -20,7 +20,7 @@ $(function() {
       $this = $("welcometotomorrow@protonmail.com");
       $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
       $.ajax({
-        url: "//formspree.io/welcometotomorrow@protonmail.com",
+        url: "welcometotomorrow@protonmail.com",
         type: "POST",
         data: {
           name: name,
