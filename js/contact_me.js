45f760b161ad9300674c5ec522123116f27103d1
GazAppy <63299214+GazAppy@users.noreply.github.com>
2020-04-21 13:15:56 +0100

    Update contact_me.js

--- js/contact_me.js
+++ js/contact_me.js
@@ -19,7 +19,7 @@ $(function() {
       }
       
       $.ajax({
-        url: "mail/contact_me.php",
+        url: "https://formspree.io/mpzjgvdq",
         type: "POST",
         data: {
           name: name,
