08e4edae56fe92e36a40302ea9e98aa931bb855f
GazAppy <63299214+GazAppy@users.noreply.github.com>
2020-04-21 14:26:38 +0100

    Update contact_me.js

--- js/contact_me.js
+++ js/contact_me.js
@@ -18,17 +18,7 @@ $(function() {
         firstName = name.split(' ').slice(0, -1).join(' ');
       }
      
-   <form id="contactForm"
-  action="https://formspree.io/mpzjgvdq"
-  method="POST"
->
-  <label>Email:</label>
-  <input type="email" name="email" />
-  <label>Message:</label>
-  <input type="text" name="message" />
-  <button id="my-form-button">Submit</button>
-  <p id="my-form-status"></p>
-</form>
+   
           $.ajax({
                 url: "https://formspree.io/mpzjgvdq",
                 type: "POST",
