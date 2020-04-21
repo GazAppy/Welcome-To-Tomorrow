60c2203e932062dc595f8b497ceb7c897571d59f
GazAppy <63299214+GazAppy@users.noreply.github.com>
2020-04-21 14:53:31 +0100

    Update contact_me.js

--- js/contact_me.js
+++ js/contact_me.js
@@ -39,7 +39,7 @@ $(function() {
                     email: email,
                     message: message},
         
-        cache: false,
+        cache: true,
         success: function() {
           // Success message
           $('#success').html("<div class='alert alert-success'>");
@@ -52,6 +52,7 @@ $(function() {
           //clear all fields
           $('#contactForm').trigger("reset");
         },
+        cache: false,
         error: function() {
           // Fail message
           $('#success').html("<div class='alert alert-danger'>");
